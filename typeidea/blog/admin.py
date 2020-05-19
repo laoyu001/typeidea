fc312f39415ddc64d71dbb1dde2289f7d5765da4
余辉 <yuhui@ctv.org.cn>
2020-05-19 13:26:54 +0800

    完成到7.1.5的内容

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,140 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.contrib.admin.models import LogEntry
 
-# Register your models here.
+from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
+from typeidea.base_admin import BaseOwnerAdmin
+from typeidea.custom_site import custom_site
+
+
+class PostInLine(admin.TabularInline):  # StackedInLine样式不同
+    fields = ('title', 'desc')
+    extra = 1  # 控制额外多几个
+    model = Post
+
+
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin):
+    inlines = [PostInLine, ]
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+    fields = ('name', 'status', 'is_nav')
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+
+    post_count.short_description = '文章数量'
+
+    # 在使用base_admin作为基类之后被替代
+    # def save_model(self, request, obj, form, change):
+    #     obj.owner = request.user
+    #     return super(CategoryAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+    # 在使用base_admin作为基类之后被替代
+    # def save_model(self, request, obj, form, change):
+    #     obj.owner = request.user
+    #     return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤器只展示当前用户分类"""
+
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+@admin.register(Post, site=custom_site)
+class PostAdmin(BaseOwnerAdmin):
+    form = PostAdminForm
+
+    list_display = (
+        'title', 'category', 'status',
+        'created_time', 'owner', 'operator'
+    )
+    list_display_links = []
+
+    list_filter = [CategoryOwnerFilter]
+    search_fields = ['title', 'category__name']
+
+    save_on_top = True
+    actions_on_top = True
+    actions_on_bottom = True
+
+    # 编辑页面
+
+    exclude = ('owner',)
+
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('collapse',),
+            'fields': ('tag',),
+        })
+    )
+
+    # 被上面的fieldsets代替
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tag',
+    # )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作'
+
+    @admin.register(LogEntry, site=custom_site)
+    class LogEntryAdmin(admin.ModelAdmin):
+        list_display = ['object_repr', 'object_id', 'action_flag', 'user', 'change_message']
+
+    # 在使用base_admin作为基类之后被替代
+    # def save_model(self, request, obj, form, change):
+    #     obj.owner = request.user
+    #     return super(PostAdmin, self).save_model(request, obj, form, change)
+    #
+    # def get_queryset(self, request):
+    #     qs = super(PostAdmin, self).get_queryset(request)
+    #     return qs.filter(owner=request.user)
+
+    # 第六章关于自定义静态资源的引入的代码，运行后页面效果还不如默认效果，所以先注视掉
+    # class Media:
+    #     css = {
+    #         'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",),
+    #     }
+    #     js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js',)
+
+
+
--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Category(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
-    STATUS_ITMES = (
+    STATUS_ITEMS = (
         (STATUS_NORMAL, '正常'),
         (STATUS_DELETE, '删除')
     )
 
     name = models.CharField(max_length=50, verbose_name="名称")
-    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITMES, verbose_name="状态")
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
     owner = models.ForeignKey(User, verbose_name="作者")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
@@ -19,29 +19,35 @@ class Category(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '分类'
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
-    STATUS_ITMES = (
+    STATUS_ITEMS = (
         (STATUS_NORMAL, '正常'),
         (STATUS_DELETE, '删除')
     )
 
     name = models.CharField(max_length=10, verbose_name="名称")
-    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITMES, verbose_name="状态")
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     owner = models.ForeignKey(User, verbose_name="作者")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
         verbose_name = verbose_name_plural = '标签'
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
     STATUS_DRAFT = 2
-    STATUS_ITMES = (
+    STATUS_ITEMS = (
         (STATUS_NORMAL, '正常'),
         (STATUS_DELETE, '删除'),
         (STATUS_DRAFT, '草稿'),
@@ -49,12 +55,15 @@ class Post(models.Model):
     title = models.CharField(max_length=255, verbose_name="标题")
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
     content = models.TextField(verbose_name="正文", help_text="正文必须为MarkDown格式")
-    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITMES, verbose_name="状态")
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     category = models.ForeignKey(Category, verbose_name="分类")
     tag = models.ManyToManyField(Tag, verbose_name="标签")
     owner = models.ForeignKey(User, verbose_name="作者")
-    creat_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
         verbose_name = verbose_name_plural = "文章"
         ordering = ['-id']    # 根据id进行降序排列
+
+    def __str__(self):
+        return self.title
